d9a15af0c9c88072b19dd72dfd3c1dbfbce6642f
Mouse <mouse-zver@xaker.ru>
2020-07-19 11:33:03 +0300

    Container

---
+++ src/Container.php
@@ -0,0 +1,41 @@
+<?php
+
+declare ( strict_types = 1 );
+
+/*
+	@ Author: MouseZver
+	@ Email: mouse-zver@xaker.ru
+	@ php-version 7.4
+*/
+
+namespace Nouvu\Container;
+
+final class Container
+{
+	private array $container;
+	
+	public function set( string $name, callable $func ): void
+	{
+		if ( isset ( $this -> container[$name] ) )
+		{
+			throw new \Error( $name . ' already exists!' );
+		}
+		
+		$this -> container[$name] = $func;
+	}
+	
+	public function get( string $name )
+	{
+		if ( ! isset ( $this -> container[$name] ) )
+		{
+			return null;
+		}
+		
+		if ( $this -> container[$name] instanceof \Closure )
+		{
+			$this -> container[$name] = $this -> container[$name]( $this );
+		}
+		
+		return $this -> container[$name];
+	}
+}
