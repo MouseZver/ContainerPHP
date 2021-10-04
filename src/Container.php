56a0d5cf68725232cba9bbd675ef3ec95a72f92a
MouseZver <mouse-zver@xaker.ru>
2021-10-04 13:42:26 +0300

    Psr\Container\ContainerInterface

--- src/Container.php
+++ src/Container.php
@@ -2,40 +2,51 @@
 
 declare ( strict_types = 1 );
 
-/*
-	@ Author: MouseZver
-	@ Email: mouse-zver@xaker.ru
-	@ php-version 7.4
-*/
+namespace Nouvu\Container;
 
-namespace Nouvu;
+use Psr\Container\ContainerInterface;
+use Closure;
 
-final class Container
+final class Container implements ContainerInterface
 {
 	private array $container;
 	
-	public function set( string $name, callable $func ): void
+	public function set( string $name, Closure $closure ): void
 	{
-		if ( isset ( $this -> container[$name] ) )
+		/* if ( isset ( $this -> container[$name] ) )
 		{
 			throw new \Error( $name . ' already exists!' );
-		}
+		} */
 		
-		$this -> container[$name] = $func;
+		$this -> container[$name] = $closure;
 	}
 	
-	public function get( string $name )
+	public function get( string $name ): mixed
 	{
-		if ( ! isset ( $this -> container[$name] ) )
-		{
-			return null;
-		}
+		$this -> has( $name ) ?: throw new ContainerException( $name );
 		
-		if ( $this -> container[$name] instanceof \Closure )
+		if ( $this -> container[$name] instanceof Closure )
 		{
 			$this -> container[$name] = $this -> container[$name]( $this );
 		}
 		
 		return $this -> container[$name];
+	}
+	
+	public function has( string $name ): bool
+	{
+		return isset ( $this -> container[$name] ) || array_key_exists ( $this -> container[$name] );
+	}
+	
+	public function make( string $name, array $params = [] ): mixed
+	{
+		if ( $this -> has( $name ) )
+		{
+			return $this -> get( $name );
+		}
+		
+		$this -> set( $name, fn( ContainerInterface $ContainerInterface ) => new $name( ...$params ) );
+		
+		return $this -> get( $name );
 	}
 }
---
+++ src/ContainerException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare ( strict_types = 1 );
+
+namespace Nouvu\Container;
+
+use Psr\Container\NotFoundExceptionInterface;
+
+class ContainerException extends \Throwable implements NotFoundExceptionInterface
+{
+	
+}
